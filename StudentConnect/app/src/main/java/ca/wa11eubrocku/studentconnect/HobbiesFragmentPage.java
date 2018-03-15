318df40d1ae8422a7f301cc98d6db6fae717b8e4
Wesley <altamirano_wes@hotmail.com>
2018-03-15 03:12:53 -0400

    updated hobbies fragment and hobbies register activity

--- StudentConnect/app/src/main/java/ca/wa11eubrocku/studentconnect/HobbiesFragmentPage.java
+++ StudentConnect/app/src/main/java/ca/wa11eubrocku/studentconnect/HobbiesFragmentPage.java
@@ -1,33 +1,34 @@
 package ca.wa11eubrocku.studentconnect;
 
-    import android.content.DialogInterface;
-    import android.os.Bundle;
-    import android.support.annotation.Nullable;
-    import android.support.v4.app.ListFragment;
-    import android.support.v7.app.AlertDialog;
-    import android.util.SparseBooleanArray;
-    import android.view.LayoutInflater;
-    import android.view.View;
-    import android.view.ViewGroup;
-    import android.widget.ArrayAdapter;
-    import android.widget.Button;
-    import android.widget.EditText;
-    import android.widget.ImageButton;
-    import java.util.ArrayList;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import java.util.ArrayList;
 
 /**
- * This class used for course page in nav drawer, user can manage their courses on this page.
+ * This class used for hobbies page in nav drawer, user can manage their hobbies on this page.
  *
- * If the user wants to add a course then they type in the course code and click add.
+ * If the user wants to add a hobby then they type in the box to search it and click add.
  *
- * If the user wants to delete a course, then they select one or more courses to remove then click
+ * If the user wants to delete a hobby, then they select one or more hobbies to remove then click
  * delete button at the bottom, and if they do this then a confirmation dialog will pop up to make
- * sure they want to delete the course, if yes then proceed with deletion.
+ * sure they want to delete the hobby, if yes then proceed with deletion.
+ *
+ * Would it be a good idea to add autofill feature to edittext box when user types beginning of interested hobby?*********************
  */
 
-public class CourseFragmentPage extends ListFragment {
+public class HobbiesFragmentPage extends ListFragment {
 
-    //courses entered by user stored here, this should change to appropriate type when we want to use the course code
     private ArrayList<String> list = new ArrayList<String>();
     private ArrayAdapter<String> adapter;
     private EditText edit;
@@ -38,14 +39,14 @@ public class CourseFragmentPage extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.courses_fragment_page, container, false);
+        View v = inflater.inflate(R.layout.hobbies_fragment_page, container, false);
 
-        ImageButton btnAdd = (ImageButton) v.findViewById(R.id.btnAddCourse);  //add course button
-        Button btnDel = (Button) v.findViewById(R.id.btnDel); //delete course button
+        ImageButton btnAdd = (ImageButton) v.findViewById(R.id.btnAddHobby);  //add hobby button
+        Button btnDel = (Button) v.findViewById(R.id.btnDel); //delete hobby button
 
-        adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),android.R.layout.simple_list_item_multiple_choice,list);   //used to set courses to ListView
+        adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),android.R.layout.simple_list_item_multiple_choice,list);   //used to set hobbies to ListView
 
-        //On click listener for Add course button
+        //On click listener for Add hobby button
         View.OnClickListener listenerAdd = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,21 +56,21 @@ public class CourseFragmentPage extends ListFragment {
             }
         };
 
-        //OnClick listener for Delete course/courses button
+        //OnClick listener for Delete hobby/hobbies button
         View.OnClickListener listenerDel = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkedItemPositions = getListView().getCheckedItemPositions();
                 itemCount = getListView().getCount();
 
-                //alert dialog pops up to confirm if user wants to delete selected course
+                //alert dialog pops up to confirm if user wants to delete selected hobby
                 AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
-                alert.setTitle("Delete Course");
-                alert.setMessage("Are you sure you want to delete this course?");
+                alert.setTitle("Delete Hobby");
+                alert.setMessage("Are you sure you want to delete this hobby?");
                 alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        //remove course from their list of courses in database
+                        //remove hobby from their list of hobbies in database
                         //continue with delete from list view
                         for(int i=itemCount-1; i >= 0; i--){
                             if(checkedItemPositions.get(i)){
@@ -80,7 +81,7 @@ public class CourseFragmentPage extends ListFragment {
                         adapter.notifyDataSetChanged();
                     }
                 });
-                //if user hits no, return to courses fragment page
+                //if user hits no, return to hobbies fragment page
                 alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -93,8 +94,8 @@ public class CourseFragmentPage extends ListFragment {
             }
         };
 
-        btnAdd.setOnClickListener(listenerAdd); //set event listener for add course button
-        btnDel.setOnClickListener(listenerDel); //set even listener for delete course button
+        btnAdd.setOnClickListener(listenerAdd); //set event listener for add hobby button
+        btnDel.setOnClickListener(listenerDel); //set even listener for delete hobby button
 
         setListAdapter(adapter);
 
@@ -106,10 +107,9 @@ public class CourseFragmentPage extends ListFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
-        getActivity().setTitle("My Courses");
+        getActivity().setTitle("My Hobbies");
 
-        //for when the user wants to add a new course, they type in the course code
-        edit = (EditText) getActivity().findViewById(R.id.courseEditItem);
+        edit = (EditText) getActivity().findViewById(R.id.hobbyEditItem);
 
     }//on ViewCreated
 
